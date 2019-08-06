b904a0a37334d3519a88cf9eece5ef8b4b4e829e
Ivan Sinkarenko <ivan.sinkarenko@cern.ch>
2019-08-06 13:56:30 +0200

    Support for dark mode

--- comrad/launcher/main.py
+++ comrad/launcher/main.py
@@ -1,5 +1,6 @@
 import os
-from typing import Any, Dict
+import sys
+from typing import Any, Dict, List
 
 
 __all__ = [
@@ -8,21 +9,25 @@ __all__ = [
 ]
 
 
-def _run_cmd(cmd: str, env: Dict[str, Any], **kwargs: Dict[str, Any]):
+_PKG_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+_DARK_FLAG = '--dark-mode'
+
+
+def _run_cmd(cmd: str, env: Dict[str, Any], args: List[str] = sys.argv, **kwargs: Dict[str, Any]):
     """
     Run a command with the given environment variables.
 
     Args:
         cmd: Name of the executable
         env: Dictionary with environment variables
+        args: Command-line arguments to read
         **kwargs: Any additional arguments to be passed to subprocess.run()
 
     Returns:
         CompletedProcess instance returned from subprocess.run()
     """
     import subprocess
-    import sys
-    sys_args = list(sys.argv)
+    sys_args = list(args)
     sys_args.pop(0)
     args = [cmd] + sys_args
     try:
@@ -40,14 +45,15 @@ def designer():
 
 def pydm():
     """Runs 'pydm' command with the environment preset to locate plugins and tools from ComRAD package."""
-    import comrad.data
-    import comrad.tools
-    path_to_plugins = os.path.abspath(os.path.dirname(comrad.data.__file__))
-    path_to_tools = os.path.abspath(os.path.dirname(comrad.tools.__file__))
+    envs = dict(PYDM_DATA_PLUGINS_PATH=os.path.join(_PKG_PATH, 'data'),
+                PYDM_DEFAULT_PROTOCOL='japc',
+                PYDM_TOOLS_PATH=os.path.join(_PKG_PATH, 'tools'))
+
+    args = sys.argv
+    if _DARK_FLAG in args:
+        envs['PYDM_STYLESHEET'] = os.path.join(_PKG_PATH, 'dark.qss')
+        args.remove(_DARK_FLAG)
 
     # The available environment variables are listed in the docs:
     # http://slaclab.github.io/pydm/configuration.html
-    _run_cmd(cmd='pydm', env=dict(PYDM_DATA_PLUGINS_PATH=path_to_plugins,
-                                  PYDM_DEFAULT_PROTOCOL='japc',
-                                  PYDM_TOOLS_PATH=path_to_tools,
-                                  ))
+    _run_cmd(cmd='pydm', env=envs, args=args)
