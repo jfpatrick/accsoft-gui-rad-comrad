1bbcb4a38eb7db3da1a5a6152a24c322e59ebfc6
Ivan Sinkarenko <ivan.sinkarenko@cern.ch>
2019-08-27 11:29:47 +0200

    Renamed examples to keep ordering. Added simple line edit example

--- comrad/examples/1_basic/2_label/__init__.py
+++ comrad/examples/1_basic/2_label/__init__.py
@@ -1,7 +1,7 @@
 title = 'Basic CLabel example'
-description = 'This example shows how to create a display with a single ComRAD label and connect it to a field of a ' \
+description = 'This example shows how to create a display with a single ComRAD line edit and connect it to a field of a ' \
               'device. Device named "DemoDevice" has a single property "Acquisition" with a single string field "Demo",' \
               'which toggles Tick-Tock labels once a second.'
 
-entrypoint = 'label.ui'
+entrypoint = 'app.ui'
 japc_generator = 'japc_device:create_device'
