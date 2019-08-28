3de9717d7fd93f48cb95e379b34963f40d7d7671
Ivan Sinkarenko <ivan.sinkarenko@cern.ch>
2019-08-28 18:42:01 +0200

    Added CShellCommand example

---
+++ comrad/examples/1_basic/12_shell_command/__init__.py
@@ -0,0 +1,6 @@
+title = 'Basic CShellCommand example'
+description = 'This example shows how to create a button that executes an external Unix command. ' \
+              'It does not require an control system configuration as the command/executable is found ' \
+              'locally in $PATH.'
+
+entrypoint = 'app.ui'
