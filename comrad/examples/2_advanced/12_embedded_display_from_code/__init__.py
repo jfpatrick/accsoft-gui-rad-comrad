2cfd85933b5409f5f0898bbd21e768f477de3a9d
Ivan Sinkarenko <ivan.sinkarenko@cern.ch>
2019-08-30 15:49:56 +0200

    Added embedded display from code example

---
+++ comrad/examples/2_advanced/12_embedded_display_from_code/__init__.py
@@ -0,0 +1,5 @@
+title = 'CEmbeddedDisplay example with included Python file'
+description = 'This example shows you how to include a Python file into an embedded display instead of Qt Designer ' \
+              'file. The use-case is identical to "Embedded Display" basic example.'
+
+entrypoint = 'app.ui'
