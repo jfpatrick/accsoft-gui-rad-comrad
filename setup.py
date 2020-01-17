1de7d741d3659a91a10ee13e90a51a0c2885ca7b
Ivan Sinkarenko <ivan.sinkarenko@cern.ch>
2020-01-17 13:59:51 +0100

    Updated pyjapc git location to fix builds. Papc taken from nexus instead of git repo

--- setup.py
+++ setup.py
@@ -24,9 +24,9 @@ requirements = {
         'argcomplete>=1.10.0&&<2',
         'colorlog>=4.0.2&&<5',
         'QtPy>=1.7&&<2',
-        'pyjapc @ git+ssh://git@gitlab.cern.ch:7999/pelson/pyjapc.git@jvm_startup_hook#egg=pyjapc',
+        'pyjapc @ git+ssh://git@gitlab.cern.ch:7999/scripting-tools/pyjapc.git',
         'accwidgets>=0.1.1&&accwidgets<1',
-        'papc @ git+ssh://git@gitlab.cern.ch:7999/pelson/papc.git#egg=papc',
+        'papc>=0.1.0&&<1',
         'pydm @ git+ssh://git@gitlab.cern.ch:7999/acc-co/accsoft/gui/rad/accsoft-gui-rad-pydm.git@multi-fix#egg=pydm',
     ],
     'test': [
