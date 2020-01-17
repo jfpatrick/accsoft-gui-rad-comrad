a8f65f50dd9107e155039ec4b25893defc38b498
Ivan Sinkarenko <ivan.sinkarenko@cern.ch>
2020-01-17 13:56:18 +0100

    Added icon test

---
+++ tests/test_icons.py
@@ -0,0 +1,16 @@
+import pytest
+from pathlib import Path
+from unittest import mock
+import comrad.icons
+
+
+@pytest.mark.parametrize('file_path,icon_name,expected_path', [
+    ('/loc1/__init__.py', 'favicon', '/loc1/icons/favicon.ico'),
+    ('/loc1/subdir1/__init__.py', 'favicon', '/loc1/subdir1/icons/favicon.ico'),
+    (None, 'favicon', f'{str(Path(comrad.icons.__file__).parent.absolute())}/favicon.ico'),
+])
+def test_load_icon(file_path, icon_name, expected_path):
+    with mock.patch('comrad.icons.QPixmap') as mocked_pixmap:
+        with mock.patch('comrad.icons.QIcon'):
+            _ = comrad.icons.icon(name=icon_name, file_path=file_path)
+            mocked_pixmap.assert_called_with(expected_path)
