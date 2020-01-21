0b3cebd97e380efdcce0aeadd05f318edcbf4bb3
Ivan Sinkarenko <ivan.sinkarenko@cern.ch>
2020-01-21 17:25:57 +0100

    Adjusted CFileTracking to work with the new PyDM file resolution logic

--- comrad/widgets/value_transform.py
+++ comrad/widgets/value_transform.py
@@ -1,40 +1,18 @@
 import logging
 import re
+import copy
 from pathlib import Path
 from string import Template
-from typing import Callable, Optional, Any
+from typing import Callable, Optional, Any, cast
 from qtpy.QtCore import Property
-from pydm.utilities import macro, is_pydm_app
+from pydm.utilities import macro, find_file
+from pydm.widgets.base import PyDMPrimitiveWidget
 
 
 logger = logging.getLogger(__name__)
 
 
 class CFileTracking:
-
-    def __init__(self):
-        """Common mixin for widgets that have to work with files and parse macros inside them."""
-        self.base_path: Optional[Path] = None
-        self.base_macros = {}
-        if is_pydm_app():
-            self.base_path = Path(self.app.directory_stack[-1])
-            self.base_macros = self.app.macro_stack[-1]
-
-    def relative_path(self, filename: str) -> Optional[Path]:
-        """
-        Finds the full path to the Python snippet.
-
-        Args:
-            filename: Filename of the Python snippet.
-
-        Returns:
-            Parsed contents of the file with substituted macros.
-        """
-        if not filename:
-            return None
-        path = Path(filename)
-        file_path = self.base_path / path if self.base_path else path
-        return file_path.expanduser().resolve()
 
     def open_file(self, full_path: Optional[Path]) -> str:
         """
@@ -58,7 +36,13 @@ class CFileTracking:
         --------
             Dictionary
         """
-        return macro.find_base_macros(self)
+        parent_display = cast(PyDMPrimitiveWidget, self).find_parent_display()
+        parent_macros = {}
+        if parent_display:
+            parent_macros = copy.copy(parent_display.macros())
+        widget_macros = macro.parse_macro_string(self.macros)
+        parent_macros.update(widget_macros)
+        return parent_macros
 
     def substituted_string(self, string: str) -> str:
         """
@@ -174,11 +158,20 @@ class CValueTransformationBase(CFileTracking):
             if self.valueTransformation:
                 parsed_code = self.substituted_string(self.valueTransformation)
                 file = None
+            elif self.snippetFilename:
+                parent_display = cast(PyDMPrimitiveWidget, self).find_parent_display()
+                base_path = None
+                if parent_display:
+                    base_path = Path(parent_display.loaded_file()).parent.absolute()
+                file_path = find_file(fname=self.snippetFilename, base_path=base_path)
+                if file_path:
+                    file = Path(file_path).absolute()
+                    parsed_code = self.open_file(file)
+                else:
+                    parsed_code = None
             else:
-                file = self.relative_path(self.snippetFilename)
-                parsed_code = self.open_file(file)
-                if file:
-                    file = file.absolute()
+                parsed_code = None
+
             if parsed_code:
                 self._value_transform_fn = _create_transformation_function(parsed_code, file=file)
         return self._value_transform_fn
@@ -220,9 +213,11 @@ __builtins__['output'] = {output_func_name}
     if file:
         global_base['__file__'] = str(file)
     del global_base['macro']
-    del global_base['CValueTransformationBase']
-    del global_base['CFileTracking']
-    del global_base['_create_transformation_function']
+    del global_base[find_file.__name__]
+    del global_base[CValueTransformationBase.__name__]
+    del global_base[CFileTracking.__name__]
+    del global_base[_create_transformation_function.__name__]
+    del global_base[PyDMPrimitiveWidget.__name__]
 
     def __comrad_dcode_wrapper__(**inputs) -> Any:
         import traceback
