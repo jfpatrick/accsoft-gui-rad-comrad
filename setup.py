5d2ac85673c69c380b438659f31c8e1850e59f78
Ivan Sinkarenko <ivan.sinkarenko@cern.ch>
2020-01-22 12:11:35 +0100

    Updated dependency version to link against correct branch of PyDM

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ requirements = {
         'pyjapc>=2.0.7&&<3',
         'accwidgets>=0.1.1&&accwidgets<1',
         'papc>=0.2.0&&<1',
-        'pydm @ git+ssh://git@gitlab.cern.ch:7999/acc-co/accsoft/gui/rad/accsoft-gui-rad-pydm.git@multi-fix#egg=pydm',
+        'pydm @ git+ssh://git@gitlab.cern.ch:7999/acc-co/accsoft/gui/rad/accsoft-gui-rad-pydm.git@multi-fix-open-file#egg=pydm',
     ],
     'test': [
         'pytest>=5.0.1&&<5.1',
