af4f438a119fd7c013e2aa5cb58cfe20020b8e86
Ivan Sinkarenko <ivan.sinkarenko@cern.ch>
2020-03-12 14:06:30 +0100

    Context can be attached to a channel and affect its address visible by PyDM

--- tests/test_channel.py
+++ tests/test_channel.py
@@ -2,7 +2,8 @@ import pytest
 from unittest import mock
 from typing import cast
 from qtpy.QtCore import Signal, QObject
-from comrad.data.channel import PyDMChannel, CChannel, allow_connections, CChannelData
+from comrad.data.channel import PyDMChannel, CChannel, format_address, CChannelData
+from comrad.data.context import CContext
 
 
 def test_equal_same():
@@ -11,44 +12,44 @@ def test_equal_same():
     assert ch1 == ch2
 
 
-def signal_factory():
-
-    class Obj(QObject):
-        sig1 = Signal()
-        sig2 = Signal()
-
-    obj = Obj()
-    yield obj.sig1
-    yield obj.sig2
-
-
-@pytest.mark.parametrize('addr1,addr2,ch1_signal_idx,ch2_signal_idx,same_request_slot,should_succeed', [
-    ('addr1', 'addr1', 0, 0, True, True),
-    ('addr1', 'addr1', 0, 0, False, False),
-    ('addr1', 'addr1', 0, 1, True, False),
-    ('addr1', 'addr1', 0, 1, False, False),
-    ('addr1', 'addr1', 0, None, True, False),
-    ('addr1', 'addr1', 0, None, False, False),
-    ('addr1', 'addr1', None, 1, True, False),
-    ('addr1', 'addr1', None, 1, False, False),
-    ('addr1', 'addr1', None, None, True, True),
-    ('addr1', 'addr1', None, None, False, False),
-    ('addr1', 'addr2', 0, 0, True, False),
-    ('addr1', 'addr2', 0, 0, False, False),
-    ('addr1', 'addr2', 0, 1, True, False),
-    ('addr1', 'addr2', 0, 1, False, False),
-    ('addr1', 'addr2', 0, None, True, False),
-    ('addr1', 'addr2', 0, None, False, False),
-    ('addr1', 'addr2', None, 1, True, False),
-    ('addr1', 'addr2', None, 1, False, False),
-    ('addr1', 'addr2', None, None, True, False),
-    ('addr1', 'addr2', None, None, False, False),
+@pytest.mark.parametrize('addr1,addr2,addr_match', [
+    ('addr1', 'addr1', True),
+    ('addr1', 'addr2', False),
 ])
-def test_equal_addresses(addr1, addr2, ch1_signal_idx, ch2_signal_idx, same_request_slot, should_succeed):
+@pytest.mark.parametrize('ch1_signal_idx,ch2_signal_idx,signals_match', [
+    (0, 0, True),
+    (0, 1, False),
+])
+@pytest.mark.parametrize('use_ctx1,use_ctx2,context_match', [
+    (True, True, True),
+    (True, True, False),
+    (False, True, False),
+    (True, False, False),
+    (False, False, True),
+])
+@pytest.mark.parametrize('same_request_slot', [True, False])
+def test_equal_addresses(addr1, addr2, addr_match, ch1_signal_idx, ch2_signal_idx, signals_match,
+                         same_request_slot, context_match, use_ctx1, use_ctx2):
+    should_succeed = same_request_slot and context_match and signals_match and addr_match
+
+    ctx1 = None if not use_ctx1 else CContext(selector='sel1')
+    ctx2 = ctx1 if context_match else (CContext(selector='sel2') if use_ctx2 else None)
     ch1 = cast(CChannel, PyDMChannel(address=addr1))
     ch2 = cast(CChannel, PyDMChannel(address=addr2))
     ch1.request_slot = mock.Mock()
+
+    def signal_factory():
+        class Obj(QObject):
+            sig1 = Signal()
+            sig2 = Signal()
+
+        obj = Obj()
+        yield obj.sig1
+        yield obj.sig2
+
     signals = list(signal_factory())
+    ch1.context = ctx1
+    ch2.context = ctx2
     ch1.request_signal = None if ch1_signal_idx is None else signals[ch1_signal_idx]
     ch2.request_signal = None if ch2_signal_idx is None else signals[ch2_signal_idx]
     ch2.request_slot = ch1.request_slot if same_request_slot else mock.Mock()
@@ -56,26 +57,6 @@ def test_equal_addresses(addr1, addr2, ch1_signal_idx, ch2_signal_idx, same_requ
     equal_revers = ch2 == ch1
     assert equal == equal_revers
     assert equal == should_succeed
-
-
-@pytest.mark.parametrize('enable_conn,should_be_called', [
-    (True, True),
-    (False, False),
-])
-def test_does_not_connect_when_connections_disabled(enable_conn, should_be_called):
-    ch = cast(CChannel, PyDMChannel(address='test'))
-    connect_mock = mock.MagicMock()
-    ch._overridden_members['connect'] = connect_mock
-    allow_connections(enable_conn)
-    ch.connect()
-    if should_be_called:
-        connect_mock.assert_called_once()
-    else:
-        connect_mock.assert_not_called()
-    connect_mock.reset_mock()
-    allow_connections(True)
-    ch.connect()
-    connect_mock.assert_called_once()
 
 
 @pytest.mark.parametrize('val1,val2,meta1,meta2,should_equal', [
@@ -90,3 +71,45 @@ def test_channel_data(val1, val2, meta1, meta2, should_equal):
     data2 = CChannelData(value=val2, meta_info=meta2)
     is_equal = data1 == data2
     assert should_equal == is_equal
+
+
+@pytest.mark.parametrize('wildcards', [None, {}, {'val1': 'key1'}])
+@pytest.mark.parametrize('addr,selector,data_filter,expected_addr', [
+    ('device/prop#field', None, None, 'device/prop#field'),
+    ('device/prop#field', 'TEST.USER.ALL', None, 'device/prop#field@TEST.USER.ALL'),
+    ('device/prop#field', '', None, 'device/prop#field'),
+    ('device/prop#field', None, {'val1': 'key1'}, 'device/prop#field?val1=key1'),
+    ('device/prop#field', None, {'val1': 'key1', 'val2': 'key2'}, 'device/prop#field?val1=key1&val2=key2'),
+    ('device/prop#field', 'TEST.USER.ALL', {'val1': 'key1'}, 'device/prop#field?val1=key1'),
+    ('device/prop#field', 'TEST.USER.ALL', {'val1': 'key1', 'val2': 'key2'}, 'device/prop#field?val1=key1&val2=key2'),
+    ('device/prop#field', '', {'val1': 'key1'}, 'device/prop#field?val1=key1'),
+    ('device/prop#field', '', {'val1': 'key1', 'val2': 'key2'}, 'device/prop#field?val1=key1&val2=key2'),
+    ('device/prop#field', None, {}, 'device/prop#field'),
+    ('device/prop#field', 'TEST.USER.ALL', {}, 'device/prop#field@TEST.USER.ALL'),
+    ('device/prop#field', '', {}, 'device/prop#field'),
+])
+def test_format_address(wildcards, addr, selector, data_filter, expected_addr):
+    actual_addr = format_address(channel_address=addr,
+                                 context=CContext(selector=selector, data_filters=data_filter, wildcards=wildcards))
+    assert actual_addr == expected_addr
+
+
+def test_address_setter():
+    ch = PyDMChannel(address='addr1')
+    context = CContext(selector='TEST.USER.ALL')
+    cast(CChannel, ch).context = context
+    with mock.patch('comrad.data.channel.format_address') as format_address:
+        ch.address = 'addr2'
+        _ = ch.address
+        format_address.assert_called_with('addr2', context)
+
+
+def test_context_setter():
+    ch = PyDMChannel(address='addr1')
+    context = CContext(selector='TEST.USER.ALL')
+    cast(CChannel, ch).context = context
+    context2 = CContext(selector='ANOTHER.USER.ALL')
+    with mock.patch('comrad.data.channel.format_address') as format_address:
+        cast(CChannel, ch).context = context2
+        _ = ch.address
+        format_address.assert_called_with('addr1', context2)
