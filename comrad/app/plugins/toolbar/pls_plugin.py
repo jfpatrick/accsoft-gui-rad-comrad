1a377233fe5c66c29d4e3a3fd3eb045fd8cd60c4
Ivan Sinkarenko <ivan.sinkarenko@cern.ch>
2020-04-07 15:04:02 +0200

    Introduced PLS toolbar plugin

---
+++ comrad/app/plugins/toolbar/pls_plugin.py
@@ -0,0 +1,164 @@
+import logging
+import os
+from typing import Optional, List, cast
+from pathlib import Path
+from qtpy.QtWidgets import QDialog, QWidget, QComboBox, QFrame, QCheckBox
+from qtpy.QtGui import QShowEvent
+from qtpy.QtCore import QStringListModel, Qt
+from qtpy.uic import loadUi
+from pyccda import SyncAPI as CCDA, sync_models as CCDATypes
+from comrad import CApplication
+from comrad.app.plugins.common import CToolbarActionPlugin
+
+
+logger = logging.getLogger(__name__)
+
+
+class PLSSelectorDialog(QDialog):
+
+    def __init__(self, parent: Optional[QWidget] = None):
+        """
+        Dialog for choosing control system "selector" on the window level.
+
+        Args:
+              parent: Owning widget.
+        """
+        super().__init__(parent)
+
+        self.machine_combo: QComboBox = None
+        self.group_combo: QComboBox = None
+        self.line_combo: QComboBox = None
+        self.chooser_frame: QFrame = None
+        self.no_selector: QCheckBox = None
+
+        loadUi(Path(__file__).parent / 'pls_dialog.ui', self)
+
+        self._original_machine: Optional[str] = None
+        self._original_group: Optional[str] = None
+        self._original_line: Optional[str] = None
+
+        self.main_window = cast(CApplication, CApplication.instance()).main_window
+
+        self.ccda = CCDA()
+        self._data: List[CCDATypes.SelectorDomain] = []
+        self.machine_combo.setModel(QStringListModel())
+        self.group_combo.setModel(QStringListModel())
+        self.line_combo.setModel(QStringListModel())
+
+        selector = self.main_window.window_context.selector
+
+        self.no_selector.setChecked(not selector)
+        self._toggle_selector(self.no_selector.checkState())
+
+        if selector:
+            machine, group, line = tuple(selector.split('.'))
+            self._original_machine = machine
+            self._original_group = group
+            self._original_line = line
+        else:
+            try:
+                machine = os.environ['PLS_TELEGRAM']
+                if machine:
+                    self._original_machine = machine
+                    self._original_group = 'USER'
+            except KeyError:
+                pass
+
+        self.no_selector.stateChanged.connect(self._toggle_selector)
+        self.accepted.connect(self._update_window_context)
+
+    def showEvent(self, event: QShowEvent):
+        super().showEvent(event)
+
+        # Have we been shown?
+        if event.spontaneous() or self._data:
+            return
+
+        # Only execute this once after being shown for the first time
+        self._data = list(self.ccda.SelectorDomain.search())
+
+        if not self._data:
+            logger.debug(f'Empty data received from CCDA. Cannot populate PLS dialog.')
+            return
+
+        cast(QStringListModel, self.machine_combo.model()).setStringList([x.name for x in self._data])
+        if self._original_machine is None:
+            machine_idx = 0
+        else:
+            machine_idx = max(0, self.machine_combo.findText(self._original_machine))
+
+        self.machine_combo.setCurrentIndex(machine_idx)
+        self._update_groups_for_machine(machine_idx)
+
+        if self._original_group is None:
+            group_idx = 0
+        else:
+            group_idx = max(0, self.group_combo.findText(self._original_group))
+
+        self.group_combo.setCurrentIndex(group_idx)
+        self._update_lines_for_group(machine_idx=machine_idx, group_idx=group_idx)
+
+        if self._original_line is None:
+            line_idx = 0
+        else:
+            line_idx = max(0, self.line_combo.findText(self._original_line))
+
+        self.line_combo.setCurrentIndex(line_idx)
+
+        self.machine_combo.currentIndexChanged[str].connect(self._machine_updated)
+        self.group_combo.currentIndexChanged[str].connect(self._group_updated)
+
+    def _machine_updated(self, text: str):
+        index = self.machine_combo.findText(text)
+        if index == -1:
+            return
+
+        self._update_groups_for_machine(index)
+        self.group_combo.setCurrentIndex(0)
+
+    def _group_updated(self, text: str):
+        index = self.group_combo.findText(text)
+        if index == -1:
+            return
+
+        self._update_lines_for_group(machine_idx=self.machine_combo.currentIndex(), group_idx=index)
+        self.line_combo.setCurrentIndex(0)
+
+    def _update_groups_for_machine(self, index: int):
+        machine = self._data[index]
+        cast(QStringListModel, self.group_combo.model()).setStringList([x.name for x in machine.selector_groups])
+
+    def _update_lines_for_group(self, machine_idx: int, group_idx: int):
+        machine = self._data[machine_idx]
+        group = machine.selector_groups[group_idx]
+        cast(QStringListModel, self.line_combo.model()).setStringList([x.name for x in group.selector_values])
+
+    def _toggle_selector(self, state: Qt.CheckState):
+        self.chooser_frame.setEnabled(state != Qt.Checked)
+
+    def _update_window_context(self):
+        if self.no_selector.isChecked():
+            self.main_window.window_context.selector = None
+        else:
+            machine = self.machine_combo.currentText()
+            group = self.group_combo.currentText()
+            line = self.line_combo.currentText()
+            if not machine or not group or not line:
+                return
+
+            self.main_window.window_context.selector = f'{machine}.{group}.{line}'
+
+
+class CycleSelectorPlugin(CToolbarActionPlugin):
+    """Plugin to display cycle selector button in the toolbar."""
+
+    plugin_id = 'comrad.pls'
+    icon = 'clock-o'  # Taken form fontawesome map available in PyDM
+    position = CToolbarActionPlugin.Position.RIGHT
+    show_in_menu = False
+
+    def triggered(self):
+        PLSSelectorDialog().exec_()
+
+    def title(self) -> str:
+        return 'PLS'
---
+++ tests/test_pls_plugin.py
@@ -0,0 +1,225 @@
+import pytest
+import os
+from typing import List, Optional, Dict
+from unittest import mock
+from comrad.app.plugins.toolbar.pls_plugin import PLSSelectorDialog
+
+
+def format_test_data(ids: List[str]) -> List[mock.MagicMock]:
+    res: Dict[str, mock.MagicMock] = {}
+
+    def mock_with_name(name: str, list_arg: Optional[str] = None) -> mock.MagicMock:
+        m = mock.MagicMock()
+        m.name = name
+        if list_arg is not None:
+            setattr(m, list_arg, {})
+        return m
+
+    for identifier in ids:
+        domain, group, line = tuple(identifier.split('.'))
+        sample_domain = res.setdefault(domain, mock_with_name(domain, list_arg='selector_groups'))
+        sample_group = sample_domain.selector_groups.setdefault(group, mock_with_name(group, list_arg='selector_values'))
+        sample_group.selector_values[line] = mock_with_name(line)
+    for domain_mock in res.values():
+        domain_mock.selector_groups = list(domain_mock.selector_groups.values())
+        for group_mock in domain_mock.selector_groups:
+            group_mock.selector_values = list(group_mock.selector_values.values())
+
+    return list(res.values())
+
+
+@pytest.fixture(autouse=True)
+def clean_env():
+    try:
+        del os.environ['PLS_TELEGRAM']
+    except KeyError:
+        pass
+
+
+@mock.patch('comrad.app.plugins.toolbar.pls_plugin.CCDA')
+def test_fetches_ccda_on_show(CCDA, qtbot):
+    CCDA.return_value.SelectorDomain.search.return_value = []
+    dialog = PLSSelectorDialog()
+    qtbot.addWidget(dialog)
+    CCDA.return_value.SelectorDomain.search.assert_not_called()
+    dialog.show()
+    CCDA.return_value.SelectorDomain.search.assert_called_once()
+
+
+@mock.patch('comrad.app.plugins.toolbar.pls_plugin.CCDA')
+def test_does_not_fetch_ccda_on_show_repeatedly(CCDA, qtbot):
+    CCDA.return_value.SelectorDomain.search.return_value = format_test_data(['TEST.USER.ALL'])
+    dialog = PLSSelectorDialog()
+    qtbot.addWidget(dialog)
+    CCDA.return_value.SelectorDomain.search.assert_not_called()
+    dialog.show()
+    CCDA.return_value.SelectorDomain.search.assert_called_once()
+    dialog.hide()
+    CCDA.return_value.SelectorDomain.search.reset_mock()
+    dialog.show()
+    CCDA.return_value.SelectorDomain.search.assert_not_called()
+
+
+@mock.patch('comrad.app.plugins.toolbar.pls_plugin.CCDA')
+def test_populates_comboboxes_with_ccda_data(CCDA, qtbot):
+    CCDA.return_value.SelectorDomain.search.return_value = format_test_data(['TEST.USER.ALL',
+                                                                             'TEST.USER.MD1',
+                                                                             'TEST.USER.MD2',
+                                                                             'TEST.CUSTOM.ALL',
+                                                                             'TEST2.USER.ALL'])
+    dialog = PLSSelectorDialog()
+    qtbot.addWidget(dialog)
+    dialog.show()
+    assert dialog.machine_combo.model().stringList() == ['TEST', 'TEST2']
+    assert dialog.group_combo.model().stringList() == ['USER', 'CUSTOM']
+    assert dialog.line_combo.model().stringList() == ['ALL', 'MD1', 'MD2']
+    dialog.group_combo.setCurrentIndex(1)
+    assert dialog.machine_combo.model().stringList() == ['TEST', 'TEST2']
+    assert dialog.group_combo.model().stringList() == ['USER', 'CUSTOM']
+    assert dialog.line_combo.model().stringList() == ['ALL']
+    dialog.group_combo.setCurrentIndex(0)
+    assert dialog.machine_combo.model().stringList() == ['TEST', 'TEST2']
+    assert dialog.group_combo.model().stringList() == ['USER', 'CUSTOM']
+    assert dialog.line_combo.model().stringList() == ['ALL', 'MD1', 'MD2']
+    dialog.machine_combo.setCurrentIndex(1)
+    assert dialog.machine_combo.model().stringList() == ['TEST', 'TEST2']
+    assert dialog.group_combo.model().stringList() == ['USER']
+    assert dialog.line_combo.model().stringList() == ['ALL']
+    dialog.machine_combo.setCurrentIndex(0)
+    assert dialog.machine_combo.model().stringList() == ['TEST', 'TEST2']
+    assert dialog.group_combo.model().stringList() == ['USER', 'CUSTOM']
+    assert dialog.line_combo.model().stringList() == ['ALL', 'MD1', 'MD2']
+    dialog.line_combo.setCurrentIndex(1)
+    assert dialog.machine_combo.model().stringList() == ['TEST', 'TEST2']
+    assert dialog.group_combo.model().stringList() == ['USER', 'CUSTOM']
+    assert dialog.line_combo.model().stringList() == ['ALL', 'MD1', 'MD2']
+    dialog.line_combo.setCurrentIndex(2)
+    assert dialog.machine_combo.model().stringList() == ['TEST', 'TEST2']
+    assert dialog.group_combo.model().stringList() == ['USER', 'CUSTOM']
+    assert dialog.line_combo.model().stringList() == ['ALL', 'MD1', 'MD2']
+    dialog.line_combo.setCurrentIndex(0)
+    assert dialog.machine_combo.model().stringList() == ['TEST', 'TEST2']
+    assert dialog.group_combo.model().stringList() == ['USER', 'CUSTOM']
+    assert dialog.line_combo.model().stringList() == ['ALL', 'MD1', 'MD2']
+
+
+@mock.patch('comrad.app.plugins.toolbar.pls_plugin.CCDA')
+def test_checkbox_toggles_all_comboboxes(CCDA, qtbot):
+    CCDA.return_value.SelectorDomain.search.return_value = format_test_data(['TEST.USER.ALL'])
+    dialog = PLSSelectorDialog()
+    qtbot.addWidget(dialog)
+    dialog.show()
+    assert dialog.no_selector.isChecked()
+    assert not dialog.machine_combo.isEnabled()
+    assert not dialog.group_combo.isEnabled()
+    assert not dialog.line_combo.isEnabled()
+    assert dialog.machine_combo.model().stringList() == ['TEST']
+    assert dialog.group_combo.model().stringList() == ['USER']
+    assert dialog.line_combo.model().stringList() == ['ALL']
+    dialog.no_selector.setChecked(False)
+    assert dialog.machine_combo.isEnabled()
+    assert dialog.group_combo.isEnabled()
+    assert dialog.line_combo.isEnabled()
+    assert dialog.machine_combo.model().stringList() == ['TEST']
+    assert dialog.group_combo.model().stringList() == ['USER']
+    assert dialog.line_combo.model().stringList() == ['ALL']
+    dialog.no_selector.setChecked(True)
+    assert not dialog.machine_combo.isEnabled()
+    assert not dialog.group_combo.isEnabled()
+    assert not dialog.line_combo.isEnabled()
+    assert dialog.machine_combo.model().stringList() == ['TEST']
+    assert dialog.group_combo.model().stringList() == ['USER']
+    assert dialog.line_combo.model().stringList() == ['ALL']
+
+
+@pytest.mark.parametrize(f'window_selector,expected_machine,expected_group,expected_line,expected_enabled', [
+    (None, 'TEST', 'USER', 'ALL', False),
+    ('', 'TEST', 'USER', 'ALL', False),
+    ('TEST.USER.ALL', 'TEST', 'USER', 'ALL', True),
+    ('TEST.USER2.ALL', 'TEST', 'USER2', 'ALL', True),
+    ('TEST.USER2.MD1', 'TEST', 'USER2', 'MD1', True),
+    ('TEST2.USER.ALL', 'TEST2', 'USER', 'ALL', True),
+    ('TEST2.USER3.ALL', 'TEST2', 'USER3', 'ALL', True),
+])
+@mock.patch('comrad.CApplication.instance')
+@mock.patch('comrad.app.plugins.toolbar.pls_plugin.CCDA')
+def test_preselects_comboboxes_based_on_window_context(CCDA, app, qtbot, window_selector, expected_enabled, expected_group, expected_line, expected_machine):
+    CCDA.return_value.SelectorDomain.search.return_value = format_test_data(['TEST.USER.ALL',
+                                                                             'TEST.USER2.ALL',
+                                                                             'TEST.USER2.MD1',
+                                                                             'TEST2.USER.ALL',
+                                                                             'TEST2.USER3.ALL'])
+    app.return_value.main_window.window_context.selector = window_selector
+    dialog = PLSSelectorDialog()
+    qtbot.addWidget(dialog)
+    dialog.show()
+    assert expected_enabled != dialog.no_selector.isChecked()
+    assert expected_enabled == dialog.machine_combo.isEnabled()
+    assert expected_enabled == dialog.group_combo.isEnabled()
+    assert expected_enabled == dialog.line_combo.isEnabled()
+    assert dialog.machine_combo.currentText() == expected_machine
+    assert dialog.group_combo.currentText() == expected_group
+    assert dialog.line_combo.currentText() == expected_line
+
+
+@pytest.mark.parametrize(f'machine,group,line,use_selector,expected_selector', [
+    ('TEST', 'USER', 'ALL', False, None),
+    ('TEST', 'USER2', 'ALL', False, None),
+    ('TEST', 'USER', 'ALL', True, 'TEST.USER.ALL'),
+    ('TEST', 'USER2', 'ALL', True, 'TEST.USER2.ALL'),
+    ('TEST', 'USER2', 'MD1', True, 'TEST.USER2.MD1'),
+    ('TEST2', 'USER', 'ALL', True, 'TEST2.USER.ALL'),
+    ('TEST2', 'USER3', 'ALL', True, 'TEST2.USER3.ALL'),
+])
+@mock.patch('comrad.CApplication.instance')
+@mock.patch('comrad.app.plugins.toolbar.pls_plugin.CCDA')
+def test_plugin_saves_context(CCDA, app, qtbot, expected_selector, machine, group, line, use_selector):
+    CCDA.return_value.SelectorDomain.search.return_value = format_test_data(['TEST.USER.ALL',
+                                                                             'TEST.USER2.ALL',
+                                                                             'TEST.USER2.MD1',
+                                                                             'TEST2.USER.ALL',
+                                                                             'TEST2.USER3.ALL'])
+    app.return_value.main_window.window_context.selector = None
+    dialog = PLSSelectorDialog()
+    qtbot.addWidget(dialog)
+    dialog.show()
+    dialog.machine_combo.setCurrentText(machine)
+    dialog.group_combo.setCurrentText(group)
+    dialog.line_combo.setCurrentText(line)
+    dialog.no_selector.setChecked(not use_selector)
+    dialog.accept()
+    assert app.return_value.main_window.window_context.selector == expected_selector
+
+
+@pytest.mark.parametrize(f'tgm_var,window_selector,expected_machine,expected_group,expected_line,expected_enabled', [
+    (None, None, 'TEST', 'USER', 'ALL', False),
+    (None, '', 'TEST', 'USER', 'ALL', False),
+    (None, 'TEST.USER2.ALL', 'TEST', 'USER2', 'ALL', True),
+    ('LHC', None, 'LHC', 'USER', 'ALL', False),
+    ('LHC', '', 'LHC', 'USER', 'ALL', False),
+    ('LHC', 'TEST.USER2.ALL', 'TEST', 'USER2', 'ALL', True),
+    ('SPS', None, 'SPS', 'PARTY', 'SPS', False),
+    ('SPS', '', 'SPS', 'PARTY', 'SPS', False),
+    ('SPS', 'TEST.USER2.ALL', 'TEST', 'USER2', 'ALL', True),
+])
+@mock.patch('comrad.CApplication.instance')
+@mock.patch('comrad.app.plugins.toolbar.pls_plugin.CCDA')
+def test_preselects_comboboxes_based_on_tgm_environment(CCDA, app, qtbot, tgm_var, window_selector, expected_enabled, expected_group, expected_line, expected_machine):
+    CCDA.return_value.SelectorDomain.search.return_value = format_test_data(['TEST.USER.ALL',
+                                                                             'TEST.USER2.ALL',
+                                                                             'LHC.SMTH.SMTH',
+                                                                             'LHC.USER.ALL',
+                                                                             'SPS.PARTY.SPS'])
+    app.return_value.main_window.window_context.selector = window_selector
+    if tgm_var:
+        os.environ['PLS_TELEGRAM'] = tgm_var
+    dialog = PLSSelectorDialog()
+    qtbot.addWidget(dialog)
+    dialog.show()
+    assert expected_enabled != dialog.no_selector.isChecked()
+    assert expected_enabled == dialog.machine_combo.isEnabled()
+    assert expected_enabled == dialog.group_combo.isEnabled()
+    assert expected_enabled == dialog.line_combo.isEnabled()
+    assert dialog.machine_combo.currentText() == expected_machine
+    assert dialog.group_combo.currentText() == expected_group
+    assert dialog.line_combo.currentText() == expected_line
