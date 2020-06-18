32be4068f565236a0d6ae132230b1504cb631ff9
Ivan Sinkarenko <ivan.sinkarenko@cern.ch>
2020-06-18 17:36:38 +0200

    Refactored Rules Editor on top of the new accwidgets code base with common style of table views

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ requirements = {
         'colorlog>=4.0.2,<5',
         'QtPy>=1.7,<2',
         'pyjapc==2.1.2',  # We must keep it pinned to this version, because comrad is obliged to override getParam, which makes it very fragile to changes
-        'accwidgets>=0.3,<0.4',
+        'accwidgets>=0.4,<0.5',
         'papc>=0.4,<0.5',
         'pydm==1.8.0.post0',
         'JPype1>=0.7,<0.7.2',  # 0.7.2 seems incompatible with pyjapc as of now
