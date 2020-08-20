397ded66d2300aa6b762d2a38d4134818454788e
Ivan Sinkarenko <ivan.sinkarenko@cern.ch>
2020-08-20 16:08:28 +0200

    Assign unique names to designer plugins to avoid possible name collisions with other packages

--- _comrad_designer/__init__.py
+++ _comrad_designer/__init__.py
@@ -1,1 +1,5 @@
 # Has to be there for package import to work
+
+# Please keep actual plugin files with unique names to prevent as many possible collisions as possible
+# within comrad and with other packages. As stated in https://issues.cern.ch/browse/ACCPY-666
+# PyQt plugin loader does not differentiate files with the same names even if they come from different directories.
