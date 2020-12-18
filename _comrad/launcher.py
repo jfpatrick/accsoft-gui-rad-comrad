ae0415523d4d0cc27258639e34e4518b6f07ad52
Ivan Sinkarenko <ivan.sinkarenko@cern.ch>
2020-12-18 10:10:12 +0100

    Avoid private argparse types in annotations

--- _comrad/launcher.py
+++ _comrad/launcher.py
@@ -3,12 +3,13 @@
 # Feature to enable argcomplete when it looks into this file
 import logging
 import argparse
-import argcomplete
 import functools
 import os
 import sys
-from typing import Optional, Tuple, Dict, List, Iterable, Any, Set
+from typing import Optional, Tuple, Dict, List, Iterable, Any, Set, cast
+from argparse import RawDescriptionHelpFormatter, ArgumentParser, Namespace
 from pathlib import Path
+from argcomplete import autocomplete
 from accwidgets.qt import exec_app_interruptable
 from .comrad_info import COMRAD_DESCRIPTION, COMRAD_VERSION, get_versions_info
 from .log_config import install_logger_level
@@ -16,7 +17,7 @@ from .common import get_japc_support_envs, comrad_asset
 from .package import generate_pyproject_with_spec, make_requirement_safe, Requirement, parse_maintainer_info
 
 
-def create_args_parser(custom_run_prog: Optional[str] = None) -> Tuple[argparse.ArgumentParser, bool]:
+def create_args_parser(custom_run_prog: Optional[str] = None) -> Tuple[ArgumentParser, bool]:
     """
     Build ComRAD application arguments.
 
@@ -38,10 +39,10 @@ def create_args_parser(custom_run_prog: Optional[str] = None) -> Tuple[argparse.
     """
     common_parser_args: Dict[str, Any] = {
         'add_help': False,  # Will be added manually (with consistent formatting)
-        'formatter_class': argparse.RawDescriptionHelpFormatter,
+        'formatter_class': RawDescriptionHelpFormatter,
     }
 
-    parser = argparse.ArgumentParser(description=logo + '\n\n' + COMRAD_DESCRIPTION, **common_parser_args)
+    parser = ArgumentParser(description=logo + '\n\n' + COMRAD_DESCRIPTION, **common_parser_args)
 
     parse_private_fn = getattr(parser, '_print_message', None)
     use_lazy_version = parse_private_fn and callable(parse_private_fn)
@@ -102,7 +103,7 @@ def create_args_parser(custom_run_prog: Optional[str] = None) -> Tuple[argparse.
     return parser, use_lazy_version
 
 
-def process_args(args: argparse.Namespace, parser: argparse.ArgumentParser, use_lazy_version: bool = False):
+def process_args(args: Namespace, parser: ArgumentParser, use_lazy_version: bool = False):
     if use_lazy_version and args.version:
         _run_version(parser)
         return
@@ -128,7 +129,7 @@ def run():
         parser, use_lazy_version = create_args_parser()
 
         # If run for auto-completion discovery, execution will stop here
-        argcomplete.autocomplete(parser)
+        autocomplete(parser)
         args = parser.parse_args()
 
         process_args(args=args,
@@ -138,13 +139,13 @@ def run():
         pass
 
 
-def _install_help(parser: argparse._ActionsContainer):
+def _install_help(parser: ArgumentParser):
     parser.add_argument('-h', '--help',
                         action='help',
                         help='Show this help message and exit.')
 
 
-def _install_controls_arguments(parser: argparse._ActionsContainer):
+def _install_controls_arguments(parser: ArgumentParser):
     parser.add_argument('-s', '--selector',
                         metavar='SELECTOR',
                         help='Default selector for the window. Selectors allow specifying the timing user, so the data '
@@ -186,14 +187,14 @@ def _install_controls_arguments(parser: argparse._ActionsContainer):
                         default=None)
 
 
-def _install_debug_arguments(parser: argparse._ActionsContainer):
+def _install_debug_arguments(parser: ArgumentParser):
     parser.add_argument('--log-level',
                         help='Configure level of log display (default: INFO).',
                         choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'],
                         default=None)
 
 
-def _run_subcommand(parser: argparse.ArgumentParser):
+def _run_subcommand(parser: ArgumentParser):
     required_group = parser.add_argument_group('Required arguments')
     required_group.add_argument('display_file',
                                 metavar='FILE',
@@ -260,7 +261,7 @@ def _run_subcommand(parser: argparse.ArgumentParser):
                                   help='Use predefined stylesheet with the dark theme for the application. '
                                        '(This option will override --stylesheet flag).')
 
-    controls_group = parser.add_argument_group('Control system configuration')
+    controls_group = cast(ArgumentParser, parser.add_argument_group('Control system configuration'))
     _install_controls_arguments(controls_group)
 
     plugin_group = parser.add_argument_group('Extensions')
@@ -324,23 +325,23 @@ def _run_subcommand(parser: argparse.ArgumentParser):
                                    'comrad.pls.show_start, comrad.pls.show_user, comrad.pls.show_lsa, '
                                    'comrad.pls.show_tz, comrad.pls.heartbeat, comrad.pls.microseconds, comrad.pls.utc.',
                               nargs=argparse.ONE_OR_MORE)
-    debug_group = parser.add_argument_group('Debugging')
+    debug_group = cast(ArgumentParser, parser.add_argument_group('Debugging'))
     _install_debug_arguments(debug_group)
     debug_group.add_argument('--perf-mon',
                              action='store_true',
                              help='Enable performance monitoring, and print CPU usage to the terminal.')
 
-    info_group = parser.add_argument_group('User information')
+    info_group = cast(ArgumentParser, parser.add_argument_group('User information'))
     _install_help(info_group)
 
 
-def _examples_subcommand(parser: argparse.ArgumentParser):
+def _examples_subcommand(parser: ArgumentParser):
     _install_debug_arguments(parser)
     _install_help(parser)
 
 
-def __designer_subcommand(parser: argparse.ArgumentParser):
-    comrad_group = parser.add_argument_group('ComRAD arguments')
+def __designer_subcommand(parser: ArgumentParser):
+    comrad_group = cast(ArgumentParser, parser.add_argument_group('ComRAD arguments'))
     comrad_group.add_argument('files',
                               help='Files to be opened in ComRAD designer.',
                               metavar='FILES',
@@ -353,7 +354,7 @@ def __designer_subcommand(parser: argparse.ArgumentParser):
     _install_debug_arguments(comrad_group)
     _install_controls_arguments(comrad_group)
 
-    qt_group = parser.add_argument_group('Standard Qt Designer arguments')
+    qt_group = cast(ArgumentParser, parser.add_argument_group('Standard Qt Designer arguments'))
     qt_group.add_argument('--server',
                           action='store_true',
                           help='Launch Qt Designer in server mode.')
@@ -370,7 +371,7 @@ def __designer_subcommand(parser: argparse.ArgumentParser):
                           help='Enable internal dynamic properties.')
 
 
-def _run_comrad(args: argparse.Namespace) -> bool:
+def _run_comrad(args: Namespace) -> bool:
     logger = logging.getLogger('')
 
     # This has to sit here, because other os.environ settings MUST be before comrad or pydm import
@@ -441,7 +442,7 @@ def _run_comrad(args: argparse.Namespace) -> bool:
     return True
 
 
-def _run_designer(args: argparse.Namespace) -> bool:
+def _run_designer(args: Namespace) -> bool:
 
     try:
         ccda_endpoint, java_env, os_env = _parse_control_env(args)
@@ -472,7 +473,7 @@ def _run_designer(args: argparse.Namespace) -> bool:
     return True
 
 
-def _run_version(parser: argparse.ArgumentParser):
+def _run_version(parser: ArgumentParser):
     versions = get_versions_info()
     version_str = f"""ComRAD {versions.comrad}
 
@@ -511,7 +512,7 @@ Environment:
     parser._print_message(version_str)
 
 
-def _parse_control_env(args: argparse.Namespace) -> Tuple[str, Dict[str, str], Dict[str, str]]:
+def _parse_control_env(args: Namespace) -> Tuple[str, Dict[str, str], Dict[str, str]]:
     from .comrad_info import CCDA_MAP
     cmw_env: str = args.cmw_env
     try:
@@ -537,7 +538,7 @@ def _parse_control_env(args: argparse.Namespace) -> Tuple[str, Dict[str, str], D
     return ccda_endpoint, jvm_flags, os_env
 
 
-def _package_subcommand(parser: argparse.ArgumentParser):
+def _package_subcommand(parser: ArgumentParser):
     _install_help(parser)
 
     parser.add_argument('--no-interactive',
@@ -576,7 +577,7 @@ def _package_subcommand(parser: argparse.ArgumentParser):
                            help='Path to the requirements.txt listing final product dependencies.')
 
 
-def _package_app(args: argparse.Namespace) -> bool:
+def _package_app(args: Namespace) -> bool:
     entrypoint = Path(args.display_file)
 
     reqs: Iterable[str]
