jfpatrick/accsoft-gui-rad-comrad

commit 0fc3d25b2198d7ffc53e377ffcd3d0526edfc9a7

Migrate from PyDM to accwidgets Qt Designer plugin base

_comrad_designer/utils.py
@@ -1,52 +1,86 @@
 """
-Utilities for Qt Designer intergation.
+Utilities for Qt Designer integration.
 """
 
-from typing import Type, List, Optional, TypeVar
+from typing import Type, List, Optional, TypeVar, Union
+from pathlib import Path
 from qtpy.QtGui import QIcon
 from qtpy.QtWidgets import QWidget
-from pydm.widgets.qtplugin_extensions import RulesExtension
-from pydm.widgets.qtplugin_base import PyDMDesignerPlugin
+from qtpy.QtDesigner import QDesignerFormEditorInterface
+from pydm.qtdesigner import DesignerHooks
+from accwidgets._designer_base import WidgetDesignerPlugin, SupportedExtensionType, WidgetBoxGroup, create_plugin
 
 
-_T = TypeVar('_T', bound=QWidget)
+CWidgetBoxGroup = WidgetBoxGroup
+CWidgetBoxGroup.VIRTUAL = 'Invisible Widgets'
 
 
-def qtplugin_factory(cls: Type[_T],
+_T = TypeVar('_T', bound=WidgetDesignerPlugin)
+
+
+def qtplugin_factory(widget_class: Type[QWidget],
+                     group: Union[str, WidgetBoxGroup],
+                     cls: Type[_T] = WidgetDesignerPlugin,
+                     extensions: Optional[List[Type[SupportedExtensionType]]] = None,
                      is_container: bool = False,
-                     icon: Optional[QIcon] = None,
-                     group: str = 'ComRAD Widgets',
-                     extensions: Optional[List[RulesExtension]] = None):
+                     tooltip: Optional[str] = None,
+                     whats_this: Optional[str] = None,
+                     icon: Optional[QIcon] = None) -> Type:
     """
-    Helper function to create a generic PyDMDesignerPlugin class.
-
-    It is similar to pydm.widgets.qtplugin_base.qtplugin_factory, but adds additional features:
-     - specifies different docstring
-     - allows specifying an icon that is visible in Qt Designer
-     - allows modifying the widget on creation in Qt Designer (useful for setting initial text)
+    Create a Qt designer plugin based on the passed widget class.
 
     Args:
-        cls: Widget class (QWidget subclass).
-        is_container: Is a container type of a widget.
-        icon: Icon as visible in the Widget Box and Object Inspector.
-        group: Category of the Widget Box, where the widget should be placed.
-        extensions: Extra extensions to apply to a widget.
+        widget_class: Widget class that the plugin should be constructed from
+        extensions: List of Extensions that the widget should have
+        is_container: whether the widget can accommodate other widgets inside
+        group: Name of the group to put widget to
+        cls: Subclass of :class:`WidgetDesignerPlugin` if you want to customize the behavior of the plugin
+        tootip: contents of the tooltip for the widget
+        whats_this: contents of the whatsThis for the widget
+        icon: Icon path as visible in the Widget Box of the Qt Designer (leave None to resolve by class name)
 
     Returns:
-        New plugin wrapper class.
+        Plugin class based on :class:`WidgetDesignerPlugin`
     """
-    class Plugin(PyDMDesignerPlugin):
-        __doc__ = 'ComRAD Designer plugin for {}'.format(cls.__name__)
+    BaseClass = create_plugin(widget_class=widget_class,
+                              group=group,
+                              cls=cls,
+                              extensions=extensions,
+                              is_container=is_container,
+                              tooltip=tooltip,
+                              whats_this=whats_this,
+                              icon_base_path=Path(__file__).absolute().parent)
 
-        def __init__(self):
-            """ComRAD Designer widget plugin wrapper."""
-            super().__init__(cls=cls, is_container=is_container, group=group, extensions=extensions, icon=icon)
+    class Plugin(BaseClass):  # type: ignore  # Avoid "Variable "BaseClass" is not valid as a type"
+        __doc__ = 'ComRAD Designer plugin for {}'.format(widget_class.__name__)
+
+        def initialize(self, core: QDesignerFormEditorInterface):
+            """
+            Override parent function to call PyDM Designer hooks, so that is_qt_designer function
+            behaves correctly.
+
+            Args:
+                core: Form editor interface to use in the initialization.
+            """
+            if not self.initialized:
+                designer_hooks = DesignerHooks()
+                designer_hooks.form_editor = core
+
+            super().initialize(core)
+
+        def icon(self):
+            if group == CWidgetBoxGroup.HIDDEN:
+                # Do not try to resolve icon (most of hidden widgets won't have it anyway)
+                return QIcon()
+            elif icon is not None:
+                return icon
+            return super().icon()
 
         def domXml(self):
             """
             XML Description of the widget's properties.
 
-            This one has tooltip removed compared to PyDM.
+            This one has tooltip removed compared to accwidgets (to avoid modified state of tooltip by default).
             """
             return (
                 '<widget class="{0}" name="{0}">\n'

_comrad_designer/comrad_designer_aux_plugin.py
@@ -52,38 +52,37 @@ from pydm.widgets.logdisplay import PyDMLogDisplay
 from pydm.widgets.enum_button import PyDMEnumButton
 from accwidgets.graph import ScrollingPlotWidget, CyclicPlotWidget, StaticPlotWidget
 from accwidgets.property_edit import PropertyEdit
-from _comrad_designer.utils import qtplugin_factory
+from _comrad_designer.utils import qtplugin_factory, CWidgetBoxGroup
 
 
 # This is a special category name hardcoded into Qt Designer (not by me, but by Qt)
 # Category with this name will not appear in the widget box
 # In Qt sources you can find it in qttools/src/designer/src/components/widgetbox/widgetboxtreewidget.cpp
 # Declared on line 70 and used on lines 613, 630
-_COMRAD_GROUP_HIDDEN_ITEMS = '[invisible]'
 
 
-_PyDMLabel = qtplugin_factory(PyDMLabel, group=_COMRAD_GROUP_HIDDEN_ITEMS)
-_PyDMCheckbox = qtplugin_factory(PyDMCheckbox, group=_COMRAD_GROUP_HIDDEN_ITEMS)
-_PyDMEnumButton = qtplugin_factory(PyDMEnumButton, group=_COMRAD_GROUP_HIDDEN_ITEMS)
-_PyDMPushButton = qtplugin_factory(PyDMPushButton, group=_COMRAD_GROUP_HIDDEN_ITEMS)
-_PyDMRelatedDisplayButton = qtplugin_factory(PyDMRelatedDisplayButton, group=_COMRAD_GROUP_HIDDEN_ITEMS)
-_PyDMShellCommand = qtplugin_factory(PyDMShellCommand, group=_COMRAD_GROUP_HIDDEN_ITEMS)
-# _PyDMWaveformTable = qtplugin_factory(PyDMWaveformTable, group=_COMRAD_GROUP_HIDDEN_ITEMS)
+_PyDMLabel = qtplugin_factory(PyDMLabel, group=CWidgetBoxGroup.HIDDEN)
+_PyDMCheckbox = qtplugin_factory(PyDMCheckbox, group=CWidgetBoxGroup.HIDDEN)
+_PyDMEnumButton = qtplugin_factory(PyDMEnumButton, group=CWidgetBoxGroup.HIDDEN)
+_PyDMPushButton = qtplugin_factory(PyDMPushButton, group=CWidgetBoxGroup.HIDDEN)
+_PyDMRelatedDisplayButton = qtplugin_factory(PyDMRelatedDisplayButton, group=CWidgetBoxGroup.HIDDEN)
+_PyDMShellCommand = qtplugin_factory(PyDMShellCommand, group=CWidgetBoxGroup.HIDDEN)
+# _PyDMWaveformTable = qtplugin_factory(PyDMWaveformTable, group=CWidgetBoxGroup.HIDDEN)
 # TODO: Uncomment if CFrame is needed
-# _PyDMFrame = qtplugin_factory(PyDMFrame, group=_COMRAD_GROUP_HIDDEN_ITEMS)
-_PyDMEmbeddedDisplay = qtplugin_factory(PyDMEmbeddedDisplay, group=_COMRAD_GROUP_HIDDEN_ITEMS)
-_PyDMTemplateRepeater = qtplugin_factory(PyDMTemplateRepeater, group=_COMRAD_GROUP_HIDDEN_ITEMS)
-_PyDMEnumComboBox = qtplugin_factory(PyDMEnumComboBox, group=_COMRAD_GROUP_HIDDEN_ITEMS)
-_PyDMLineEdit = qtplugin_factory(PyDMLineEdit, group=_COMRAD_GROUP_HIDDEN_ITEMS)
-_PyDMSlider = qtplugin_factory(PyDMSlider, group=_COMRAD_GROUP_HIDDEN_ITEMS)
-_PyDMSpinbox = qtplugin_factory(PyDMSpinbox, group=_COMRAD_GROUP_HIDDEN_ITEMS)
-_PyDMByteIndicator = qtplugin_factory(PyDMByteIndicator, group=_COMRAD_GROUP_HIDDEN_ITEMS)
-# _PyDMImageView = qtplugin_factory(PyDMImageView, group=_COMRAD_GROUP_HIDDEN_ITEMS)
-_PyDMLogDisplay = qtplugin_factory(PyDMLogDisplay, group=_COMRAD_GROUP_HIDDEN_ITEMS)
-_PyDMScaleIndicator = qtplugin_factory(PyDMScaleIndicator, group=_COMRAD_GROUP_HIDDEN_ITEMS)
-# _PyDMTabWidget = qtplugin_factory(PyDMTabWidget, group=_COMRAD_GROUP_HIDDEN_ITEMS)
+# _PyDMFrame = qtplugin_factory(PyDMFrame, group=CWidgetBoxGroup.HIDDEN)
+_PyDMEmbeddedDisplay = qtplugin_factory(PyDMEmbeddedDisplay, group=CWidgetBoxGroup.HIDDEN)
+_PyDMTemplateRepeater = qtplugin_factory(PyDMTemplateRepeater, group=CWidgetBoxGroup.HIDDEN)
+_PyDMEnumComboBox = qtplugin_factory(PyDMEnumComboBox, group=CWidgetBoxGroup.HIDDEN)
+_PyDMLineEdit = qtplugin_factory(PyDMLineEdit, group=CWidgetBoxGroup.HIDDEN)
+_PyDMSlider = qtplugin_factory(PyDMSlider, group=CWidgetBoxGroup.HIDDEN)
+_PyDMSpinbox = qtplugin_factory(PyDMSpinbox, group=CWidgetBoxGroup.HIDDEN)
+_PyDMByteIndicator = qtplugin_factory(PyDMByteIndicator, group=CWidgetBoxGroup.HIDDEN)
+# _PyDMImageView = qtplugin_factory(PyDMImageView, group=CWidgetBoxGroup.HIDDEN)
+_PyDMLogDisplay = qtplugin_factory(PyDMLogDisplay, group=CWidgetBoxGroup.HIDDEN)
+_PyDMScaleIndicator = qtplugin_factory(PyDMScaleIndicator, group=CWidgetBoxGroup.HIDDEN)
+# _PyDMTabWidget = qtplugin_factory(PyDMTabWidget, group=CWidgetBoxGroup.HIDDEN)
 
-_AccScrollingPlot = qtplugin_factory(ScrollingPlotWidget, group=_COMRAD_GROUP_HIDDEN_ITEMS)
-_AccCyclicPlot = qtplugin_factory(CyclicPlotWidget, group=_COMRAD_GROUP_HIDDEN_ITEMS)
-_AccStaticPlot = qtplugin_factory(StaticPlotWidget, group=_COMRAD_GROUP_HIDDEN_ITEMS)
-_PropertyEdit = qtplugin_factory(PropertyEdit, group=_COMRAD_GROUP_HIDDEN_ITEMS)
+_AccScrollingPlot = qtplugin_factory(ScrollingPlotWidget, group=CWidgetBoxGroup.HIDDEN)
+_AccCyclicPlot = qtplugin_factory(CyclicPlotWidget, group=CWidgetBoxGroup.HIDDEN)
+_AccStaticPlot = qtplugin_factory(StaticPlotWidget, group=CWidgetBoxGroup.HIDDEN)
+_PropertyEdit = qtplugin_factory(PropertyEdit, group=CWidgetBoxGroup.HIDDEN)

_comrad_designer/comrad_designer_widgets_plugin.py
@@ -6,7 +6,6 @@ Plugins for Qt Designer that are visible ComRAD widgets.
 import signal
 signal.signal(signal.SIGINT, signal.SIG_DFL)
 
-import functools
 import accwidgets.property_edit.designer
 import accwidgets.led.designer
 from pathlib import Path
@@ -18,19 +17,19 @@ from _comrad_designer.log_config import setup_logging
 setup_logging()
 
 # from pydm.widgets.tab_bar_qtplugin import TabWidgetPlugin as PyDMTabWidgetPlugin
-from pydm.widgets.qtplugin_extensions import PyDMExtension
 from accwidgets.property_edit.designer.designer_extensions import PropertyFieldExtension
+from accwidgets._designer_base import WidgetsTaskMenuExtension
 from comrad import (CScrollingPlot, CCyclicPlot, CStaticPlot, CValueAggregator, CCommandButton, CScaleIndicator, CLogDisplay,
                     CEnumComboBox, CSlider, CSpinBox, CLabel, CByteIndicator, CLineEdit, CTemplateRepeater,
                     CEmbeddedDisplay, CShellCommand, CRelatedDisplayButton, CPushButton, CEnumButton, CCheckBox,
                     CPropertyEdit, CLed, CContextFrame)
 from comrad.icons import icon
-from _comrad_designer.utils import qtplugin_factory
+from _comrad_designer.utils import qtplugin_factory, CWidgetBoxGroup
 from _comrad_designer.rules_editor import RulesEditor
 from _comrad_designer.graphs import CPlottingItemEditorExtension, CLayerEditorExtension
 
 
-class _RulesExtension(PyDMExtension):
+class _RulesExtension(WidgetsTaskMenuExtension):
 
     def __init__(self, widget: QWidget):
         """
@@ -51,71 +50,70 @@ class _RulesExtension(PyDMExtension):
         RulesEditor(widget=self.widget, parent=None).exec_()
 
 
-_load_icon = functools.partial(icon, file_path=__file__)
-
-
 _BASE_EXTENSIONS = [_RulesExtension]
 _PLOT_EXTENSIONS = [CPlottingItemEditorExtension, CLayerEditorExtension]
 
 
-# Currently the groups are made so that new widgets blend into the standard PyQt widgets
-_COMRAD_GROUP_CONTAINER = 'Containers'  # 'ComRAD Container Widgets'
-_COMRAD_GROUP_DISPLAY = 'Display Widgets'  # 'ComRAD Display Widgets'
-_COMRAD_GROUP_INPUT = 'Input Widgets'  # 'ComRAD Input Widgets'
-_COMRAD_GROUP_PLOT = 'Charts'  # 'ComRAD Plot Widgets'
-_COMRAD_GROUP_BUTTONS = 'Buttons'
-_COMRAD_GROUP_ITEM_VIEWS = 'Item Widgets (Item-Based)'
-_COMRAD_GROUP_VIRTUAL = 'Invisible Widgets'
-
-
 # Buttons
-_CCheckbox = qtplugin_factory(CCheckBox, group=_COMRAD_GROUP_BUTTONS, icon=_load_icon('checkbox'), extensions=_BASE_EXTENSIONS)
+_CCheckbox = qtplugin_factory(CCheckBox, group=CWidgetBoxGroup.BUTTONS, extensions=_BASE_EXTENSIONS)
 
 
-_CEnumButton = qtplugin_factory(CEnumButton, group=_COMRAD_GROUP_BUTTONS, icon=_load_icon('enum_btn'), extensions=_BASE_EXTENSIONS)
-_CPushButton = qtplugin_factory(CPushButton, group=_COMRAD_GROUP_BUTTONS, icon=_load_icon('push_btn'), extensions=_BASE_EXTENSIONS)
-_CCommandButton = qtplugin_factory(CCommandButton, group=_COMRAD_GROUP_BUTTONS, icon=_load_icon('cmd_btn'))
-_CRelatedDisplayButton = qtplugin_factory(CRelatedDisplayButton, group=_COMRAD_GROUP_BUTTONS, icon=_load_icon('related_display'))
-_CShellCommand = qtplugin_factory(CShellCommand, group=_COMRAD_GROUP_BUTTONS, icon=_load_icon('shell_cmd'))
-# _CToggleButton = qtplugin_factory(CToggleButton, group=_COMRAD_GROUP_BUTTONS, icon=load_icon('toggle'), extensions=_BASE_EXTENSIONS)
+_CEnumButton = qtplugin_factory(CEnumButton, group=CWidgetBoxGroup.BUTTONS, extensions=_BASE_EXTENSIONS)
+_CPushButton = qtplugin_factory(CPushButton, group=CWidgetBoxGroup.BUTTONS, extensions=_BASE_EXTENSIONS)
+_CCommandButton = qtplugin_factory(CCommandButton, group=CWidgetBoxGroup.BUTTONS)
+_CRelatedDisplayButton = qtplugin_factory(CRelatedDisplayButton, group=CWidgetBoxGroup.BUTTONS)
+_CShellCommand = qtplugin_factory(CShellCommand, group=CWidgetBoxGroup.BUTTONS)
+# _CToggleButton = qtplugin_factory(CToggleButton, group=CWidgetBoxGroup.BUTTONS, extensions=_BASE_EXTENSIONS)
 
 # Item Widgets
 # TODO: Uncomment when useful
-# _CWaveformTable = qtplugin_factory(CWaveFormTable, group=_COMRAD_GROUP_ITEM_VIEWS, icon=_load_icon('waveform_table'), extensions=_BASE_EXTENSIONS)
+# _CWaveformTable = qtplugin_factory(CWaveFormTable, group=CWidgetBoxGroup.ITEM_WIDGETS, extensions=_BASE_EXTENSIONS)
 
 # Containers
 # TODO: What is CFrame useful for?
-# _CFrame = qtplugin_factory(CFrame, group=_COMRAD_GROUP_CONTAINER, icon=load_icon('frame'), is_container=True, extensions=_BASE_EXTENSIONS)
-_CEmbeddedDisplay = qtplugin_factory(CEmbeddedDisplay, group=_COMRAD_GROUP_CONTAINER, icon=_load_icon('embedded_display'))
-_CTemplateRepeater = qtplugin_factory(CTemplateRepeater, group=_COMRAD_GROUP_CONTAINER, icon=_load_icon('template_repeater'))
-_CContextFrame = qtplugin_factory(CContextFrame, group=_COMRAD_GROUP_CONTAINER, icon=_load_icon('context_frame'), is_container=True)
+# _CFrame = qtplugin_factory(CFrame, group=CWidgetBoxGroup.CONTAINERS, is_container=True, extensions=_BASE_EXTENSIONS)
+_CEmbeddedDisplay = qtplugin_factory(CEmbeddedDisplay, group=CWidgetBoxGroup.CONTAINERS)
+_CTemplateRepeater = qtplugin_factory(CTemplateRepeater, group=CWidgetBoxGroup.CONTAINERS)
+_CContextFrame = qtplugin_factory(CContextFrame, group=CWidgetBoxGroup.CONTAINERS, is_container=True)
 
 # Input Widgets
-_CEnumComboBox = qtplugin_factory(CEnumComboBox, group=_COMRAD_GROUP_INPUT, icon=_load_icon('combobox'), extensions=_BASE_EXTENSIONS)
-_CLineEdit = qtplugin_factory(CLineEdit, group=_COMRAD_GROUP_INPUT, icon=_load_icon('line_edit'), extensions=_BASE_EXTENSIONS)
-_CSlider = qtplugin_factory(CSlider, group=_COMRAD_GROUP_INPUT, icon=_load_icon('slider'), extensions=_BASE_EXTENSIONS)
-_CSpinbox = qtplugin_factory(CSpinBox, group=_COMRAD_GROUP_INPUT, icon=_load_icon('spinbox'), extensions=_BASE_EXTENSIONS)
+_CEnumComboBox = qtplugin_factory(CEnumComboBox, group=CWidgetBoxGroup.INPUTS, extensions=_BASE_EXTENSIONS)
+_CLineEdit = qtplugin_factory(CLineEdit, group=CWidgetBoxGroup.INPUTS, extensions=_BASE_EXTENSIONS)
+_CSlider = qtplugin_factory(CSlider, group=CWidgetBoxGroup.INPUTS, extensions=_BASE_EXTENSIONS)
+_CSpinbox = qtplugin_factory(CSpinBox, group=CWidgetBoxGroup.INPUTS, extensions=_BASE_EXTENSIONS)
 _CPropertyEdit = qtplugin_factory(CPropertyEdit,
-                                  group=_COMRAD_GROUP_INPUT,
+                                  group=CWidgetBoxGroup.INPUTS,
                                   icon=icon('PropertyEdit', file_path=Path(accwidgets.property_edit.designer.__file__)),
                                   extensions=[PropertyFieldExtension, *_BASE_EXTENSIONS])
 
 # Display Widgets
-_CLabel = qtplugin_factory(CLabel, group=_COMRAD_GROUP_DISPLAY, icon=_load_icon('label'), extensions=_BASE_EXTENSIONS)
-_CByteIndicator = qtplugin_factory(CByteIndicator, group=_COMRAD_GROUP_DISPLAY, icon=_load_icon('byte_indicator'), extensions=_BASE_EXTENSIONS)
+_CLabel = qtplugin_factory(CLabel, group=CWidgetBoxGroup.INDICATORS, extensions=_BASE_EXTENSIONS)
+_CByteIndicator = qtplugin_factory(CByteIndicator, group=CWidgetBoxGroup.INDICATORS, extensions=_BASE_EXTENSIONS)
 # TODO: Uncomment when useful
-# _CImageView = qtplugin_factory(CImageView, group=_COMRAD_GROUP_DISPLAY, icon=_load_icon('image_view'), extensions=_BASE_EXTENSIONS)
-_CLogDisplay = qtplugin_factory(CLogDisplay, group=_COMRAD_GROUP_DISPLAY, icon=_load_icon('log_viewer'), extensions=_BASE_EXTENSIONS)
-_CScaleIndicator = qtplugin_factory(CScaleIndicator, group=_COMRAD_GROUP_DISPLAY, icon=_load_icon('scale_indicator'), extensions=_BASE_EXTENSIONS)
-_CEnumLed = qtplugin_factory(CLed, group=_COMRAD_GROUP_DISPLAY, icon=icon('Led', file_path=Path(accwidgets.led.designer.__file__)), extensions=_BASE_EXTENSIONS)
+# _CImageView = qtplugin_factory(CImageView, group=CWidgetBoxGroup.INDICATORS, extensions=_BASE_EXTENSIONS)
+_CLogDisplay = qtplugin_factory(CLogDisplay, group=CWidgetBoxGroup.INDICATORS, extensions=_BASE_EXTENSIONS)
+_CScaleIndicator = qtplugin_factory(CScaleIndicator, group=CWidgetBoxGroup.INDICATORS, extensions=_BASE_EXTENSIONS)
+_CEnumLed = qtplugin_factory(CLed,
+                             group=CWidgetBoxGroup.INDICATORS,
+                             icon=icon('Led', file_path=Path(accwidgets.led.designer.__file__)),
+                             extensions=_BASE_EXTENSIONS)
 
 # Charts
-_CScrollingPlot = qtplugin_factory(CScrollingPlot, group=_COMRAD_GROUP_PLOT, icon=_load_icon('graph_scrolling_plot'), extensions=_PLOT_EXTENSIONS)
-_CCyclicPlot = qtplugin_factory(CCyclicPlot, group=_COMRAD_GROUP_PLOT, icon=_load_icon('graph_sliding_plot'), extensions=_PLOT_EXTENSIONS)
-_CStaticPlot = qtplugin_factory(CStaticPlot, group=_COMRAD_GROUP_PLOT, icon=_load_icon('graph_static_plot'), extensions=_PLOT_EXTENSIONS)
+_CScrollingPlot = qtplugin_factory(CScrollingPlot,
+                                   group=CWidgetBoxGroup.CHARTS,
+                                   icon=icon('ScrollingPlotWidget', file_path=Path(accwidgets.graph.designer.__file__)),
+                                   extensions=_PLOT_EXTENSIONS)
+_CCyclicPlot = qtplugin_factory(CCyclicPlot,
+                                group=CWidgetBoxGroup.CHARTS,
+                                icon=icon('CyclicPlotWidget', file_path=Path(accwidgets.graph.designer.__file__)),
+                                extensions=_PLOT_EXTENSIONS)
+_CStaticPlot = qtplugin_factory(CStaticPlot,
+                                group=CWidgetBoxGroup.CHARTS,
+                                icon=icon('StaticPlotWidget', file_path=Path(accwidgets.graph.designer.__file__)),
+                                extensions=_PLOT_EXTENSIONS)
 
 # Invisible
-_CValueAggregator = qtplugin_factory(CValueAggregator, group=_COMRAD_GROUP_VIRTUAL, icon=_load_icon('calc'))
+_CValueAggregator = qtplugin_factory(CValueAggregator, group=CWidgetBoxGroup.VIRTUAL)
 
 
 # # Tab Widget plugin
@@ -129,10 +127,7 @@ _CValueAggregator = qtplugin_factory(CValueAggregator, group=_COMRAD_GROUP_VIRTU
 #         # This needs to be done via init, because event with the change to PyDMTabWidgetPlugin,
 #         # group appeared to be unchanged.
 #         super().__init__(extensions=extensions)
-#         self._group = _COMRAD_GROUP_CONTAINER
-#
-#     def icon(self):
-#         return load_icon('tab_widget')
+#         self._group = WidgetBoxGroup.CONTAINERS
 #
 #
 # TabWidget = TabWidgetPlugin(extensions=_BASE_EXTENSIONS)

commit 1cf53d9872b4615de9d6cc5f18e414adc40e433b

Fix regression introduced in 0fc3d25b2198d7ffc53e377ffcd3d0526edfc9a7 that prevented init_for_designer method being invoked

_comrad_designer/utils.py
@@ -54,6 +54,8 @@ def qtplugin_factory(widget_class: Type[QWidget],
     class Plugin(BaseClass):  # type: ignore  # Avoid "Variable "BaseClass" is not valid as a type"
         __doc__ = 'ComRAD Designer plugin for {}'.format(widget_class.__name__)
 
+        CUSTOM_INITIALIZER_METHOD = 'init_for_designer'
+
         def initialize(self, core: QDesignerFormEditorInterface):
             """
             Override parent function to call PyDM Designer hooks, so that is_qt_designer function